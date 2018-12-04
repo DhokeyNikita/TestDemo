595945eb1372f0af417e358ce0ea733e82952761
DhokeyNikita <nikitadhokey94@gmail.com>
2018-12-04 10:54:19 +0530

    By nikita

--- Project/src/test/java/TestMaven/Project/AppTest.java
+++ Project/src/test/java/TestMaven/Project/AppTest.java
@@ -6,93 +6,94 @@ import junit.framework.TestCase;
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+extends TestCase
 {
-	
-	
 	@org.testng.annotations.Test
 	public void testABC()
 	{
-		System.out.println( "Hello World!" );
+		for(int i =0; i<=5;i++)
+		{
+			System.out.println( "Hello World!" );
+		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
- /*   
-  * Create the test case
-    *
-     * @param testName name of the test case
-     
-     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*   
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+
+
     public AppTest( String testName )
     {
         super( testName );
     }
 
 
-    *//**
-     * @return the suite of tests being tested
-     *//*
+	 *//**
+	 * @return the suite of tests being tested
+	 *//*
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
 
-    *//**
-     * Rigourous Test :-)
-     *//*
+	  *//**
+	  * Rigourous Test :-)
+	  *//*
     public void testApp()
     {
         assertTrue( true );
     }*/
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
